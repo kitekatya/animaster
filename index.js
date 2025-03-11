25cabdd9a2d0e7cbd951f006759e2b1feb5c0f02
kitekatya <prostopostya221@yandex.ru>
2025-03-11 16:16:25 +0500

    kek

--- index.js
+++ index.js
@@ -30,6 +30,50 @@ function addListeners() {
             const block = document.getElementById('moveAndHideBlock');
             animaster().moveAndHide(block, 3000);
         });
+
+    document.getElementById('moveAndHideReset')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().resetMoveAndScale(block);
+            animaster().resetFadeOut(block);
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 3000);
+        });
+
+    let heartBeatingInstance;
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            heartBeatingInstance = animaster().heartBeating(block);
+        });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            if (heartBeatingInstance) {
+                heartBeatingInstance.stop();
+            }
+        });
+
+    document.getElementById('customAnimationPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('customAnimationBlock');
+            const customAnimation = animaster()
+                .addMove(200, {x: 40, y: 40})
+                .addScale(800, 1.3)
+                .addMove(200, {x: 80, y: 0})
+                .addScale(800, 1)
+                .addMove(200, {x: 40, y: -40})
+                .addScale(800, 0.7)
+                .addMove(200, {x: 0, y: 0})
+                .addScale(800, 1);
+            customAnimation.play(block);
+        });
+
+
 }
 
 function animaster() {
@@ -62,5 +106,73 @@ function animaster() {
             this.move(element, (2 / 5) * duration, { x: 100, y: 20 });
             setTimeout(() => this.fadeOut(element, (3 / 5) * duration), (2 / 5) * duration);
         },
+
+        showAndHide(element, duration) {
+            this.fadeIn(element, duration / 3);
+            setTimeout(() => this.fadeOut(element, duration / 3), (2 / 3) * duration);
+        },
+        
+
+        heartBeating(element) {
+            let growing = true;
+            const interval = setInterval(() => {
+                if (growing) {
+                    this.scale(element, 500, 1.4);
+                } else {
+                    this.scale(element, 500, 1);
+                }
+                growing = !growing;
+            }, 500);
+            return {
+                stop() {
+                    clearInterval(interval);
+                }
+            };
+        },
+
+        addMove(duration, translation) {
+            this._steps.push({ type: 'move', duration, translation });
+            return this;
+        },
+
+        addScale(duration, ratio) {
+            this._steps.push({ type: 'scale', duration, ratio });
+            return this;
+        },
+
+        addFadeIn(duration) {
+            this._steps.push({ type: 'fadeIn', duration });
+            return this;
+        },
+
+        addFadeOut(duration) {
+            this._steps.push({ type: 'fadeOut', duration });
+            return this;
+        },
+
+        resetMoveAndScale(element) {
+            element.style.transition = null;
+            element.style.transform = null;
+        },
+
+        resetFadeOut(element) {
+            element.style.transition = null;
+            element.classList.remove('hide');
+            element.classList.add('show');
+        },
+
+        play(element) {
+            let delay = 0;
+            this._steps.forEach(step => {
+                setTimeout(() => {
+                    if (step.type === 'move') this.move(element, step.duration, step.translation);
+                    if (step.type === 'scale') this.scale(element, step.duration, step.ratio);
+                    if (step.type === 'fadeIn') this.fadeIn(element, step.duration);
+                    if (step.type === 'fadeOut') this.fadeOut(element, step.duration);
+                }, delay);
+                delay += step.duration;
+            });
+        }
     };
 }
+
